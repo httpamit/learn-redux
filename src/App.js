9d00a7e3ad707a57bacbd3ccf9ea57a21b2d2b63
Amit Gupta <amgupta@mafcarrefour.com>
2019-10-11 14:41:33 +0400

    file update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useSelector, useDispatch } from 'react-redux';
+import { increment, decrement } from './actions';
 
 function App() {
+  const { counter, isLogged } = useSelector(state => state);
+  const dispatch = useDispatch();
+
+  const handleIncrement = () => {
+    dispatch(increment(5))
+  };
+
+  const handleDecrement = () => {
+    dispatch(decrement(2))
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <h1>Counter {counter}</h1>
+      <button onClick={handleIncrement}>+</button>
+      <button onClick={handleDecrement}>-</button>
+      {isLogged &&
         <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+          Valuable information
+      </p>}
     </div>
   );
 }
